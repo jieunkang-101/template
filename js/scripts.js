99a4f7fceaa44bbb9ecf6621a0c5609269280239
Jieun Kang <jieunkang101@gmail.com>
2020-01-24 07:37:40 -0800

    create template for project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// business logic
+
+
+
+
+
+// user interface logic
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+ 
+
+
+       
+    $("#result").show();    
+  });
+});
